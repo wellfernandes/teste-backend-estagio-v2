2fec2da06ffa71bc41e6c4e6a35b79df9b950914
Welliton Fernandes Leal <wellfernandes@me.com>
2022-06-21 17:52:01 -0300

    project created

---
+++ equipament-data-collection-api/src/test/java/wellfernandes/com/github/equipamentdatacollectionapi/EquipamentDataCollectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package wellfernandes.com.github.equipamentdatacollectionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipamentDataCollectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
