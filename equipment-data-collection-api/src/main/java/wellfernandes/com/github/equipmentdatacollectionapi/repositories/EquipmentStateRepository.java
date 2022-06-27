d0306529979502e7b898d093600443eb98c0de85
Welliton Fernandes Leal <wellfernandes@me.com>
2022-06-27 19:43:02 -0300

    equipment state repository added

---
+++ equipment-data-collection-api/src/main/java/wellfernandes/com/github/equipmentdatacollectionapi/repositories/EquipmentStateRepository.java
@@ -0,0 +1,11 @@
+package wellfernandes.com.github.equipmentdatacollectionapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import wellfernandes.com.github.equipmentdatacollectionapi.entities.EquipmentState;
+
+@Repository
+public interface EquipmentStateRepository extends JpaRepository<EquipmentState, Long> {
+
+}
