5f44afd42eb7e22ae4a9d1f7abd85f122596a951
Welliton Fernandes Leal <wellfernandes@me.com>
2022-06-27 16:19:29 -0300

    endpoint to update equipment added

--- equipment-data-collection-api/src/main/java/wellfernandes/com/github/equipmentdatacollectionapi/resources/EquipmentResources.java
+++ equipment-data-collection-api/src/main/java/wellfernandes/com/github/equipmentdatacollectionapi/resources/EquipmentResources.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +50,15 @@ public class EquipmentResources {
 	public ResponseEntity<Void> deleteEquipment(@PathVariable Long id) {
 		equipmentService.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Equipment> updateEquipment(@PathVariable Long id, @RequestBody Equipment equipment) {
+		try {
+			Equipment newEquipment = equipmentService.updateEquipment(id, equipment);
+			return ResponseEntity.ok(newEquipment);
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
